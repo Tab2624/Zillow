aea13e5be75d00b814f481ed47fac1ff28c5b59a
Tab2624 <tabodansky@gmail.com>
2023-09-18 03:51:50 -0400

    Project Zillow

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.getElementById('btnRight').addEventListener("click", scroll())
+
+
+
+// function scroll(){
+//     alert('hello');
+//     // scrollBy(30, 0); // for right scroll
+//     // scrollBy(-30, 0); // for left scroll
+// }
+
+
+// function scrollRight(idToSelect){
+//     var rem = document.querySelector(idToSelect);
+//     alert('hello')
+//     // scrollBy(30,0)
+// }
+
+function scroll(element){ 
+    var right = document.getElementById('#btnRight')
+    alert('hello')
+    scroll(30,0)
+}
