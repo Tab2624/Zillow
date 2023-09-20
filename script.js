7810a2e5fbc5e09902ede9ceddb49ca925a9ed64
Tab2624 <tabodansky@gmail.com>
2023-09-20 16:42:11 -0400

    nav dropdown

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function scrollToRight(idToSelect) {
-    var element = document.getElementById(idToSelect);
-    console.log(element)
-    element.scrollLeft += 500;
+  var element = document.getElementById(idToSelect);
+  console.log(element);
+  element.scrollLeft += 500;
 }
 function scrollToLeft(idToSelect) {
-    var element = document.getElementById(idToSelect);
-    console.log(element)
-    element.scrollLeft -= 500;
+  var element = document.getElementById(idToSelect);
+  console.log(element);
+  element.scrollLeft -= 500;
 }
 
-var searchItem = ''
-function search(elem){
-    searchItem = elem.value;
+var searchItem = "";
+function search(elem) {
+  searchItem = elem.value;
 }
 
-function clickSearch(elem){
-    window.open(`https://www.zillow.com/homes/${searchItem}_rb/`)
+function clickSearch(elem) {
+  window.open(`https://www.zillow.com/homes/${searchItem}_rb/`);
 }
 
 
+// document.getElementById('buyDropdown').onclick = function() {myFunction()};
 
+function myFunction(idToSelect){
+  document.getElementById(idToSelect).classList.toggle("show")
+}
 
+function onmouseLeave(idToSelect){
+  document.getElementById(idToSelect).classList.toggle('show')
+}
