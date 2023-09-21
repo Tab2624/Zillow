680208880d6141e4a853a5a9de90dc6994c88ad2
Tab2624 <tabodansky@gmail.com>
2023-09-21 05:00:18 -0400

    Started work on Modal

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function scrollToRight(idToSelect) {
   var element = document.getElementById(idToSelect);
   console.log(element);
-  element.scrollLeft += 500;
+  element.scrollLeft += 900;
 }
 function scrollToLeft(idToSelect) {
   var element = document.getElementById(idToSelect);
   console.log(element);
-  element.scrollLeft -= 500;
+  element.scrollLeft -= 900;
 }
 
 var searchItem = "";
@@ -37,3 +37,37 @@ input.addEventListener("keypress", function(event) {
     document.getElementById("searchIcon").click();
   }
 });
+
+function scrollDown(idToSelect){
+  document.getElementById("scrollDownBtn")
+  window.scrollTo(0, 1100)
+}
+
+
+
+// Modal Stuff
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
